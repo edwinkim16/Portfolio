ed2f76cece9b3bed4f89d71db66347105b4ca288
edwinkim16 <edwinkimathi226@gmail.com>
2025-09-07 17:09:01 +0300

    main items setup

---
+++ main.js
@@ -0,0 +1,7 @@
+// Simple form submission demo
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! Your message has been sent.');
+    this.reset();
+});
